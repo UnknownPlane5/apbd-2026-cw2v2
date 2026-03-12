9bbb8bb72fa5b3b71e24203519e7daaee7f40a72
UnknownPlane5 <78647561+UnknownPlane5@users.noreply.github.com>
2026-03-12 09:36:44 +0100

    Trzeci commit do zadania 2.Dodano 3 liczby oraz utworzono klase StatistcsHelper.

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -4,6 +4,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Feature2!");
+        int a = 20;
+        int b = 10;
+        int d = 0;
+        
+    }
+
+    class StatisticsHelper(int a, int b)
+    {
+        
     }
 }
 
