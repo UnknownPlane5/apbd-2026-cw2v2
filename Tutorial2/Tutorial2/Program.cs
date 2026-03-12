055fbe8662aef7a18aecad49af3e7af65eee42b8
UnknownPlane5 <78647561+UnknownPlane5@users.noreply.github.com>
2026-03-12 09:26:46 +0100

    Drugi commit do zadania 2. Usunięto wszystkie "Hello World"

--- Tutorial2/Tutorial2/Program.cs
+++ Tutorial2/Tutorial2/Program.cs
@@ -3,11 +3,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        
-        Console.WriteLine("Hello, World!");
         Console.WriteLine("Feature2!");
-        Console.WriteLine("Hello, World!");
     }
 }
 
